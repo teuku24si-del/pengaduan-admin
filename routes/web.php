7d3dd8015d4444cda1b9a9357424f4738b738d6a
hasbipcr <teuku24si@mahasiswa.pcr.ac.id>
2025-09-23 07:52:24 +0700

    setup awal

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MatakuliahController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+
+Route::get('/mahasiswa', function () {
+    return 'Halo Mahasiswa';
+})->name('mahasiswa.show');
+
+Route::get('/nama/{hasbi}', function ($hasbi) {
+    return 'Nama saya: ' . $hasbi;
+});
+
+Route ::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
+
+Route :: get('/matakuliah/{param1}', [MatakuliahController::class, 'show']);
+
+
