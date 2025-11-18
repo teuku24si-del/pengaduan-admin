307e214100805c845a877f5ddcd6c17fff29b85b
hasbipcr <teuku24si@mahasiswa.pcr.ac.id>
2025-11-18 07:30:28 +0700

    paginate dan penambahan vendor folder bootstrap 5

--- resources/views/admin/pelanggan/index.blade.php
+++ resources/views/admin/pelanggan/index.blade.php
@@ -31,53 +31,56 @@
         </div>
     </div>
 
-     <div class="row">
-            <div class="col-12 mb-4">
-                <div class="card border-0 shadow mb-4">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table id="table-pelanggan" class="table table-centered table-nowrap mb-0 rounded">
-                                <thead class="thead-light">
+    <div class="row">
+        <div class="col-12 mb-4">
+            <div class="card border-0 shadow mb-4">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="table-pelanggan" class="table table-centered table-nowrap mb-0 rounded">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th class="border-0">First Name</th>
+                                    <th class="border-0">Last Name</th>
+                                    <th class="border-0">Birthday</th>
+                                    <th class="border-0">Gender</th>
+                                    <th class="border-0">Email</th>
+                                    <th class="border-0">Phone</th>
+                                    <th class="border-0 rounded-end">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($dataPelanggan as $item)
                                     <tr>
-                                        <th class="border-0">First Name</th>
-                                        <th class="border-0">Last Name</th>
-                                        <th class="border-0">Birthday</th>
-                                        <th class="border-0">Gender</th>
-                                        <th class="border-0">Email</th>
-                                        <th class="border-0">Phone</th>
-                                        <th class="border-0 rounded-end">Action</th>
+                                        <td>{{ $item->firstname }}</td>
+                                        <td>{{ $item->last_name }}</td>
+                                        <td>{{ $item->birhday }}</td>
+                                        <td>{{ $item->gender }}</td>
+                                        <td>{{ $item->email }}</td>
+                                        <td>{{ $item->phone }}</td>
+
+                                        <td><a href="{{ route('pelanggan.edit', $item->pelanggan_id) }}"
+                                                class="btn btn-info btn-sm">
+                                                <svg class="icon icon-xs me-2" data-slot="icon" fill="none"
+                                                    stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24"
+                                                    xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10">
+                                                    </path>
+                                                </svg>
+                                                Edit
+                                            </a></td>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($dataPelanggan as $item)
-                                        <tr>
-                                            <td>{{ $item->firstname }}</td>
-                                            <td>{{ $item->last_name }}</td>
-                                            <td>{{ $item->birhday }}</td>
-                                            <td>{{ $item->gender }}</td>
-                                            <td>{{ $item->email }}</td>
-                                            <td>{{ $item->phone }}</td>
+                                @endforeach
 
-                                            <td><a href="{{ route('pelanggan.edit', $item->pelanggan_id) }}"
-                                                    class="btn btn-info btn-sm">
-                                                    <svg class="icon icon-xs me-2" data-slot="icon" fill="none"
-                                                        stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24"
-                                                        xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10">
-                                                        </path>
-                                                    </svg>
-                                                    Edit
-                                                </a></td>
-                                        </tr>
-                                    @endforeach
-
-                                </tbody>
-                            </table>
+                            </tbody>
+                        </table>
+                        <div class="mt-3">
+                            {{ $dataPelanggan->links('pagination::bootstrap-5') }}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     {{-- end main content --}}
 @endsection
